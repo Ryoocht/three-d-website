286c2945649cf5016473a7703005359e849e3412
Ryoocht <ryo1.t.zubu@gmail.com>
2023-04-05 21:55:13 +1000

    added: camera and shadows

--- src/canvas/CameraRig.tsx
+++ src/canvas/CameraRig.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { Group } from "three";
+import state from "../store";
 
-type Props = {}
+type CameraRigProps = {
+  children: JSX.Element;
+};
 
-const CameraRig = (props: Props) => {
-  return (
-    <div>CameraRig</div>
-  )
-}
+const CameraRig: React.FC<CameraRigProps> = ({ children }) => {
+  const group = useRef<Group>(undefined);
+  const snap = useSnapshot(state);
 
-export default CameraRig
+  useFrame((state, delta) => {
+    const isBreakPoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // set the initial position of the model
+    let targetPosition = [-0.4, 0, 2] as [x: number, y: number, z: number];
+    if (snap.intro) {
+      if (isBreakPoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    // set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+    // set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
+};
+
+export default CameraRig;
